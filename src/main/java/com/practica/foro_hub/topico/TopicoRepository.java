1e351acd1c85af0855030e0a2ada7f25e36dea3d
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-07-10 20:45:15 -0600

    Creaicon de metodo para llamar topicos ordenados por fecha

--- src/main/java/com/practica/foro_hub/topico/TopicoRepository.java
+++ src/main/java/com/practica/foro_hub/topico/TopicoRepository.java
@@ -1,6 +1,9 @@
 package com.practica.foro_hub.topico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    Page<Topico> findAllByOrderByFechaDeCreacionAsc(Pageable paginacion);
 }
