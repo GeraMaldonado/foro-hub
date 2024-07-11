251963e8d8346b645225040372860497f38b7b84
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-07-10 19:28:08 -0600

    Creacion de record para recibir datos del topico

---
+++ src/main/java/com/practica/foro_hub/topico/DatosRegistrarTopico.java
@@ -0,0 +1,22 @@
+package com.practica.foro_hub.topico;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DatosRegistrarTopico(
+        @NotNull @NotBlank
+        String titulo,
+        @NotNull @NotBlank
+        String mensaje,
+        @NotNull
+        LocalDateTime fechaDeCreacion,
+        @NotNull @NotBlank
+        String status,
+        @NotNull @NotBlank
+        String autor,
+        @NotNull @NotBlank
+        String curso
+) {
+}
